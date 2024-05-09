5caad407923204514eb766e40bc85ff169890d0d
b.zakaria <zakaria_bouzangad@gmail.Com>
2024-05-09 13:27:50 +0100

    Navigating Programmatically

--- app/routes/expenses.add.tsx
+++ app/routes/expenses.add.tsx
@@ -1,10 +1,17 @@
+import { useNavigate } from "@remix-run/react";
 import ExpenseForm from "~/components/expenses/ExpenseForm";
 import Modal from "~/components/util/Modal";
 
 export default function ExpensesAdd() {
-    return (
-      <Modal onClose={null}>
-        <ExpenseForm />
-      </Modal>
-    )
-  }
+  const navigate = useNavigate();
+
+  return (
+    <Modal
+      onClose={() => {
+        navigate("..");
+      }}
+    >
+      <ExpenseForm />
+    </Modal>
+  );
+}
