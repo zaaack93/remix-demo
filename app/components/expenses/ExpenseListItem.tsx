7d247ba72c9ef8809cda2e9f51bb03121edc7779
b.zakaria <zakaria_bouzangad@gmail.Com>
2024-05-16 15:06:30 +0100

    Programmatic BehindtheScenes Submission via useFetcher

--- app/components/expenses/ExpenseListItem.tsx
+++ app/components/expenses/ExpenseListItem.tsx
@@ -1,8 +1,20 @@
-import { Form, Link } from "@remix-run/react";
+import { Link, useFetcher } from "@remix-run/react";
 
 function ExpenseListItem({ id, title, amount }) {
+  const fetcher = useFetcher({ key: "add-to-bag" });
+
   function deleteExpenseItemHandler() {
     // tbd
+    const proceed = confirm("Are you sure you want to delete this expense?");
+    console.log(proceed,id);
+    if (proceed) {
+      fetcher.submit(null,{
+        method: "DELETE",
+        action: `/expenses/${id}`,
+      });
+    }
+
+
 
   }
 
@@ -13,9 +25,7 @@ function ExpenseListItem({ id, title, amount }) {
         <p className="expense-amount">${amount.toFixed(2)}</p>
       </div>
       <menu className="expense-actions">
-        <Form method="DELETE" action={`/expenses/${id}`}>
-          <button>Delete</button>
-        </Form>
+        <button onClick={deleteExpenseItemHandler}>Delete</button>
         <Link to={id}>Edit</Link>
       </menu>
     </article>
